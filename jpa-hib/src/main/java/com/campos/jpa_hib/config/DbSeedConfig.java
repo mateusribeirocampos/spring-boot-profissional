7c9376df303082f8cc24617561eedf92bf963181
mateusribeirocampos <mateusrcampos@gmail.com>
2025-10-16 15:32:32 -0300

    Chore: Order class received @OneToMany that was mappedBy = id.order and was implemented getItems to Set<OrderItem>. OrderItemRepository was implemented and called in the DbConfig

--- jpa-hib/src/main/java/com/campos/jpa_hib/config/DbSeedConfig.java
+++ jpa-hib/src/main/java/com/campos/jpa_hib/config/DbSeedConfig.java
@@ -1,14 +1,8 @@
 package com.campos.jpa_hib.config;
 
-import com.campos.jpa_hib.entities.Category;
-import com.campos.jpa_hib.entities.Order;
-import com.campos.jpa_hib.entities.Product;
-import com.campos.jpa_hib.entities.User;
+import com.campos.jpa_hib.entities.*;
 import com.campos.jpa_hib.entities.enums.OrderStatus;
-import com.campos.jpa_hib.repositories.CategoryRepository;
-import com.campos.jpa_hib.repositories.OrderRepository;
-import com.campos.jpa_hib.repositories.ProductRepository;
-import com.campos.jpa_hib.repositories.UserRepository;
+import com.campos.jpa_hib.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -37,6 +31,9 @@ public class DbSeedConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -74,10 +71,15 @@ public class DbSeedConfig implements CommandLineRunner {
         Order o5 = new Order(null, Instant.parse("2025-10-13T17:58:11Z"), OrderStatus.WAITING_PAYMENT, u2 );
         Order o6 = new Order(null, Instant.parse("2025-10-13T18:03:26Z"), OrderStatus.SHIPPED, u3 );
 
-
-
         userRepository.saveAll(Arrays.asList(u1, u2, u3));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4, o5, o6));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
     }
 }
--- jpa-hib/src/main/java/com/campos/jpa_hib/entities/pk/OrderItemPk.java
+++ jpa-hib/src/main/java/com/campos/jpa_hib/entities/pk/OrderItemPk.java
@@ -24,6 +24,8 @@ public class OrderItemPk implements Serializable {
     @JoinColumn(name = "product_id")
     private Product product;
 
+    public OrderItemPk() {}
+
     public Order getOrder() {
         return order;
     }
