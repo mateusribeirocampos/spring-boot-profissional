4a8d1a3894e68eac4df40b71c4c3bd85aad945f8
mateusribeirocampos <mateusrcampos@gmail.com>
2025-10-31 17:14:03 -0300

    Chore: Classe Participante, Atividade Bloco instanciadas - Atividade e Participante associacao @ManyToMany - Bloco com associacao @ManyToOne com Atividade e inversa com Bloco - Banco de dados em memoria implementado com profile dev

---
+++ mdorm/src/main/java/com/campos/mdorm/entities/Atividade.java
@@ -0,0 +1,98 @@
+package com.campos.mdorm.entities;
+
+import jakarta.persistence.*;
+
+import java.util.*;
+
+@Entity
+@Table(name = "tb_atividades")
+public class Atividade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    private Double price;
+
+    @ManyToMany(mappedBy = "atividades")
+    private Set<Participante> participantes = new HashSet<>();
+
+    @OneToMany(mappedBy = "atividade")
+    private List<Bloco> blocos = new ArrayList<>();
+
+    public Atividade() {}
+
+    public Atividade(Integer id, String nome, String descricao, Double price, Set<Participante> participantes, List<Bloco> blocos) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.price = price;
+        this.participantes = participantes;
+        this.blocos = blocos;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Set<Participante> getParticipantes() {
+        return participantes;
+    }
+
+    public void setParticipantes(Set<Participante> participantes) {
+        this.participantes = participantes;
+    }
+
+    public List<Bloco> getBlocos() {
+        return blocos;
+    }
+
+    public void setBlocos(List<Bloco> blocos) {
+        this.blocos = blocos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Atividade that = (Atividade) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
--- mdorm/src/main/java/com/campos/mdorm/entities/Atividades.java
+++
@@ -1,9 +0,0 @@
-package com.campos.mdorm.entities;
-
-public class Atividades {
-
-    private Integer id;
-    private String nome;
-    private String descricao;
-    private Double price;
-}
