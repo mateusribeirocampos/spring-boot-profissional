b6e4233971e296ed81ccdd24e7dd64696e554969
mateusribeirocampos <mateusrcampos@gmail.com>
2025-11-13 16:01:31 -0300

    Chore: findAll implemented using pageable and parameter (size and desc)

--- ecommerce/src/main/java/com/campos/ecommerce/controllers/ProductController.java
+++ ecommerce/src/main/java/com/campos/ecommerce/controllers/ProductController.java
@@ -1,16 +1,17 @@
 package com.campos.ecommerce.controllers;
 
 import com.campos.ecommerce.dto.ProductDto;
-import com.campos.ecommerce.entities.Product;
-import com.campos.ecommerce.repositories.ProductRepository;
 import com.campos.ecommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -23,5 +24,10 @@ public class ProductController {
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ProductDto findById(@PathVariable Long id) {
         return productService.findById(id);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Page<ProductDto> findAll(Pageable pageable) {
+        return productService.findAll(pageable);
     }
 }
--- ecommerce/src/main/java/com/campos/ecommerce/services/ProductService.java
+++ ecommerce/src/main/java/com/campos/ecommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.campos.ecommerce.dto.ProductDto;
 import com.campos.ecommerce.entities.Product;
 import com.campos.ecommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -29,6 +32,12 @@ public class ProductService {
         ProductDto dto = new ProductDto(product);
         System.out.println("dto: " + dto.getName());
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDto> findAll(Pageable pageable) {
+        Page<Product> productList = productRepository.findAll(pageable);
+        return productList.map(x -> new ProductDto(x));
     }
 
 }
