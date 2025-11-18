bbfc89cdace72ec9d248b12c6127fb162afe0c65
mateusribeirocampos <mateusrcampos@gmail.com>
2025-11-18 09:41:00 -0300

    Chore: save PersonDepartmentDto

--- 04JPA/jpa-sql-jpql/oneToOne/src/main/java/com/campos/onetoone/services/PersonService.java
+++ 04JPA/jpa-sql-jpql/oneToOne/src/main/java/com/campos/onetoone/services/PersonService.java
@@ -4,6 +4,7 @@ import com.campos.onetoone.controllers.PersonController;
 import com.campos.onetoone.dto.PersonDepartmentDto;
 import com.campos.onetoone.entities.Department;
 import com.campos.onetoone.entities.Person;
+import com.campos.onetoone.repositories.DepartmentRepository;
 import com.campos.onetoone.repositories.PersonRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +17,9 @@ public class PersonService {
     private Logger logger = LoggerFactory.getLogger(PersonController.class);
 
     @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
     private PersonRepository personRepository;
 
     public PersonDepartmentDto create(PersonDepartmentDto dto) {
@@ -24,8 +28,10 @@ public class PersonService {
         entity.setName(dto.getName());
         entity.setSalary(dto.getSalary());
 
-        Department dept = new Department();
-        dept.setId(dto.getDepartment().getId());
+//        Department dept = new Department();
+        Department dept = departmentRepository.getReferenceById(dto.getDepartment().getId());
+
+//        dept.setId(dto.getDepartment().getId());
 
         // association
         entity.setDepartment(dept);
