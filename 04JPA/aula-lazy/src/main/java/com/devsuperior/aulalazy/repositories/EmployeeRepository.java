6621f642c5ef261193c38a47e32a8311bcd31c1a
mateusribeirocampos <mateusrcampos@gmail.com>
2025-11-19 16:07:20 -0300

    Fix: convert aula-lazy from broken submodule to regular directory

---
+++ 04JPA/aula-lazy/src/main/java/com/devsuperior/aulalazy/repositories/EmployeeRepository.java
@@ -0,0 +1,18 @@
+package com.devsuperior.aulalazy.repositories;
+
+import java.util.List;
+
+import com.devsuperior.aulalazy.dto.EmployeeMinDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.aulalazy.entities.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+	@Query("SELECT obj FROM Employee obj JOIN FETCH obj.department")
+	List<Employee> findEmployeesWithDepartments();
+
+    List<Employee> findByNameContainingIgnoreCase(String name);
+
+}
