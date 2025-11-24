e16253737dec0057642bf0ad7ebe61833013acb3
mateusribeirocampos <mateusrcampos@gmail.com>
2025-11-24 18:17:27 -0300

    Fix: directory rearenged

--- 04JPA/jpa-sql-jpql/oneToOne/src/main/java/com/campos/onetoone/repositories/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.campos.onetoone.repositories;
-
-import com.campos.onetoone.entities.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
--- 04JPA/jpa-sql-jpql/oneToOne/src/main/java/com/campos/onetoone/repositories/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.campos.onetoone.repositories;
-
-import com.campos.onetoone.entities.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
