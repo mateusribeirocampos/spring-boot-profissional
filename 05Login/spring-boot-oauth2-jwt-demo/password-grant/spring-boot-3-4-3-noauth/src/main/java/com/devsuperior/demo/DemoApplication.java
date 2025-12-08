83db8260e0f1f372a656f2d18606e7670720a4a1
mateusribeirocampos <mateusrcampos@gmail.com>
2025-12-08 14:37:20 -0300

    Chore: spring security added in the pom.xml and passwordEncoder implemented in the config

--- 05Login/spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ 05Login/spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODER: " + passwordEncoder.encode("123456"));
+
+		boolean result = passwordEncoder.matches("123456","$2a$10$U.LQuIqa8Z9au3tpm2IvFe.8gc8I6FOLLA38CaXsXdbYscCH.xmJW");
+		System.out.println("RESULTADO: " + result );
+
+	}
 }
